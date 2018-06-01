a11a0790ac943b66e52e14ce86dff72e26d04d8a
Lam <daniel.2.lam@atos.net>
2018-06-01 20:20:19 +0100

    Allow use of https in local development environment
    Create an https server when running in local development environment and the "ENV" variable is set to "development".

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 import { Logger } from "@hmcts/nodejs-logging";
+import * as fs from "fs";
+import * as https from "https";
+import * as path from "path";
 import { app } from "./app";
 
 const logger = Logger.getLogger("server");
@@ -7,6 +10,18 @@ const logger = Logger.getLogger("server");
 // TODO: set the right port for your application
 const port = process.env.PORT || "3100";
 
-app.listen(port, () => {
-  logger.info(`Application started: http://localhost:${port}`);
-});
+if (app.locals.ENV === "development") {
+  const sslDirectory = path.join(__dirname, "resources", "localhost-ssl");
+  const sslOptions = {
+    cert: fs.readFileSync(path.join(sslDirectory, "localhost.crt")),
+    key: fs.readFileSync(path.join(sslDirectory, "localhost.key")),
+  };
+  const server = https.createServer(sslOptions, app);
+  server.listen(port, () => {
+    logger.info(`Application started: https://localhost:${port}`);
+  });
+} else {
+  app.listen(port, () => {
+    logger.info(`Application started: http://localhost:${port}`);
+  });
+}
