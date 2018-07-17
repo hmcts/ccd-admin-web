dda27cbee42fc4925d8d4f2f1ea3ef030cb3b6dd
Mohammed Anas Khatri <mohammedanaskhatri@Mohammeds-MacBook-Pro.local>
2018-07-17 10:39:26 +0100

    Create user service implementation

--- src/main/routes/create.user.profile.ts
+++ src/main/routes/create.user.profile.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
 import { fetchAll } from "../service/jurisdiction.service";
+import { createUserProfile } from "../service/create-user-service";
+import { UserProfile } from "../domain/userprofile";
 
 const router = express.Router();
 
-/* GET Jurisdiction page. */
+/* GET create user form. */
 router.get("/createuser", (req, res, next) => {
 
   fetchAll(req).then((response) => {
@@ -18,5 +20,17 @@ router.get("/createuser", (req, res, next) => {
     });
 });
 
+/* POST create user result. */
+router.post("/createuser", (req, res, next) => {
+  createUserProfile(req, new UserProfile(req.body.idamsId,
+    req.body.jurisdictionDropdown, req.body.caseTypeDropdown, req.body.stateDropdown))
+    .then((response) => {
+      res.status(201);
+      res.render("jurisdictions", { sucess: "Creating user profile" });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
 /* tslint:disable:no-default-export */
 export default router;
