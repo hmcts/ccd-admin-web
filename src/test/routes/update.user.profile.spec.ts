515b3b24ebf922787fe7e2c8f7bc0043366b6b1c
Mohammed Anas Khatri <mohammed.khatri@hmcts.net>
2018-08-09 16:53:08 +0100

    Added update user profile test

---
+++ src/test/routes/update.user.profile.spec.ts
@@ -0,0 +1,38 @@
+import { app } from "../../main/app";
+import { expect } from "chai";
+import * as idamServiceMock from "../http-mocks/idam";
+import * as mock from "nock";
+import * as request from "supertest";
+import { get } from "config";
+
+describe("on Get /updateusersprofile", () => {
+    beforeEach(() => {
+        mock.cleanAll();
+    });
+    it("Update user should redirect to IdAM login page when not authenticated", () => {
+        return request(app)
+            .get("/updateusersprofile")
+            .then((res) => {
+                expect(res.statusCode).to.equal(302);
+                expect(res.headers.location.startsWith(get("adminWeb.login_url"))).to.be.true;
+            });
+    });
+
+    it("should respond with update user form and populated response when authenticated", () => {
+        idamServiceMock.resolveRetrieveUserFor("1", "admin");
+        idamServiceMock.resolveRetrieveServiceToken();
+
+        mock("http://localhost:4451")
+            .get("/api/data/jurisdictions")
+            .reply(200, [{ id: "jd_1", name: "Jurisdiction 1" }, { id: "jd_2", name: "Jurisdiction 2" }]);
+
+        return request(app)
+            .get("/updateusersprofile")
+            .set("Cookie", "accessToken=ey123.ey456")
+            .then((res) => {
+                expect(res.statusCode).to.equal(201);
+                expect(res.text).to.contain("Jurisdiction 1");
+                expect(res.text).to.contain("Jurisdiction 2");
+            });
+    });
+});
