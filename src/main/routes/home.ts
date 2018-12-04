b48eccaae6ce09115021c47934e5213d9f40a346
Adrian Cho <waihung.cho@hmcts.net>
2018-12-04 14:49:21 +0000

    RDM-1956 Add to home page import audits

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,10 +1,37 @@
 import * as express from "express";
+import { fetch } from "../service/get-service";
+import * as config from "config";
+import { Logger } from "@hmcts/nodejs-logging";
+
+const url = config.get("adminWeb.import_audits_url");
 
 const router = express.Router();
 
+const logger = Logger.getLogger(__filename);
+
 /* GET home page. */
 router.get("/import", (req, res, next) => {
-  res.render("home");
+  fetch(req, url).then((response) => {
+    res.status(201);
+    const responseContent: { [k: string]: any } = {};
+    logger.info(`response 5: ${responseContent}`);
+
+    responseContent.importAudits = JSON.parse(response);
+    logger.info(`response 6: ${responseContent.importAudits}`);
+    if (req.query.page) {
+      delete req.session.error;
+    }
+    if (req.session.error) {
+      responseContent.error = req.session.error;
+      delete req.session.error;
+    }
+    res.render("home", responseContent);
+  })
+    .catch((error) => {
+      // Call the next middleware, which is the error handler
+      next(error);
+    });
+  // res.render("home");
 });
 
 router.get("/", (req, res, next) => {
