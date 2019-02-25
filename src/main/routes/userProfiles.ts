9f22d21f9369cf7540d91fcaddff561b83278997
Adrian Cho <waihung.cho@hmcts.net>
2019-02-25 13:20:29 +0000

    RDM-4013 Restict access to manage user profile

--- src/main/routes/userProfiles.ts
+++ src/main/routes/userProfiles.ts
@@ -9,16 +9,24 @@ const userProfilesPage = "user-profiles";
 /* POST */
 router.post("/userprofiles", validate, (req, res, next) => {
 
-  const query = { jurisdiction: req.body.jurisdictionName };
-  render(req, res, next, url, query, userProfilesPage);
+  if (req.adminWebAuthorization.canManageUserProfile) {
+    const query = {jurisdiction: req.body.jurisdictionName};
+    render(req, res, next, url, query, userProfilesPage);
+  } else {
+    res.render(userProfilesPage);
+  }
 });
 
 /* GET */
 router.get("/userprofiles", (req, res, next) => {
 
-  // Jurisdiction is expected to be set already on the session, hence it can be used for the query
-  const query = req.session.jurisdiction ? { jurisdiction: req.session.jurisdiction } : {};
-  render(req, res, next, url, query, userProfilesPage);
+  if (req.adminWebAuthorization.canManageUserProfile) {
+    // Jurisdiction is expected to be set already on the session, hence it can be used for the query
+    const query = req.session.jurisdiction ? { jurisdiction: req.session.jurisdiction } : {};
+    render(req, res, next, url, query, userProfilesPage);
+  } else {
+    res.render(userProfilesPage);
+  }
 });
 
 export default router;
