07935d5a4ad0cad3bfd6b9153f1eb890d024df17
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2020-03-03 15:15:33 +0000

    RDM-7636 - migrate admin-web to oidc

--- src/main/user/user-resolver.ts
+++ src/main/user/user-resolver.ts
@@ -4,7 +4,7 @@ import { get } from "config";
 export const getTokenDetails = (jwt) => {
   const BEARER_JWT = jwt.startsWith("Bearer ") ? jwt : "Bearer " + jwt;
 
-  return fetch(`${get("idam.base_url")}/details`, {
+  return fetch(`${get("idam.base_url")}/o/userinfo`, {
     headers: {
       Authorization: BEARER_JWT,
     },
