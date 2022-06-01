5055338a36e3f24102f525a644c3df10bcdb99e0
markdathorne <mark.dathorne@cgi.com>
2022-06-01 16:29:01 +0100

    RDM-13210 Extract dictionary entries from csv & send to Translation service.

--- src/main/service/manage-welsh-dictionary-service.ts
+++ src/main/service/manage-welsh-dictionary-service.ts
@@ -1,44 +1,79 @@
 import * as config from "config";
 import { Logger } from "@hmcts/nodejs-logging";
 import * as request from "superagent";
-//import { csvToJson } from "convert-csv-to-json";
+const logger = Logger.getLogger(__filename);
 
-export function uploadFile(req) {
+const fs = require("fs");
+const csv = require("fast-csv");
+
+function buildTranslationsJson(data) {
+  let translations = "";
+  for (const element of data) {
+      if (translations.length > 0) {
+          translations += ",";
+      }
+      translations += "\"" + element.englishPhrase + "\":\"" + element.welshPhrase + "\"";
+  }
+  return translations;
+}
+
+function getRowDataArrayFromCsv(stream) {
+    return new Promise((resolve, reject) => {
+        const data = [];
+        csv
+            .parseStream(stream, {headers : true})
+            .on("error", reject)
+            .on("data", (row) => {
+                data.push(row);
+            })
+            .on("end", () => {
+                resolve(data);
+            });
+    });
+}
+
+export async function uploadFile(req) {
   const url = config.get("adminWeb.welsh_translation_get_dictionary_url");
+  const filePath = config.get("adminWeb.welsh_translation_upload_dictionary_file_path");
   const headers = {
-    Authorization: req.accessToken,
-    ServiceAuthorization: req.serviceAuthToken,
+      "Accept": "application/json",
+      "Authorization": req.accessToken,
+      "Content-Type": "application/json",
+      "ServiceAuthorization": req.serviceAuthToken,
   };
-  const logger = Logger.getLogger(__filename);
 
- const dictionary = {};
-// const translations = {};
-//  dictionary.push(translations);
-//  const json = csvToJson.fieldDelimiter(',').getJsonFromCsv(req.file.buffer);
-//  dictionary.translations.push(json);
- console.log(dictionary);
+  const fileRef = filePath + req.file.originalname;
+  const stream = fs.createReadStream(fileRef);
+  logger.info("fileRef: " + fileRef);
 
- var obj = '{'
-        +'"name" : "Raj",'
-        +'"age"  : 32,'
-        +'"married" : false'
-        +'}';
-        console.log(obj);
-    return request
-        .post(url)
+  const data = await getRowDataArrayFromCsv(stream);
+  logger.debug("data:", data);
+
+  const translations = buildTranslationsJson(data);
+
+  const dictionary = "{\"translations\":{" + translations + "}}";
+  logger.debug("dictionary: {}", dictionary);
+
+  logger.info("Putting dictionary...");
+
+  return request
+        .put(url)
         .set(headers)
         .set("enctype", "multipart/form-data")
-        .attach("file", req.file.buffer, { filename: req.file.originalname })
+        .send(dictionary)
         .then((res) => {
-          logger.info(`Uploaded file ${req.file.originalname} to Translation Service, response: ${res.text}`);
+          logger.debug("Dictionary uploaded from file " + fileRef + " to Translation Service, response: " + res.text);
+          if (res.text == null) {
+            res.text = "Dictionary successfully updated";
+          }
           return res;
         })
         .catch((error) => {
           if (error.response) {
-            logger.error(`Error uploading file to Translation Service: ${error.response.text}`);
+            logger.error(`Error uploading Dictionary data to Translation Service: ${error.response.text}`);
             throw error;
           } else {
-            const errMsg = "Error uploading file to Translation Service: no error response";
+            const errMsg = "Error uploading Dictionary data to Translation Service: no error response";
             logger.error(errMsg);
             error.text = errMsg;
             throw error;
