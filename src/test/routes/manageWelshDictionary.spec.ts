05312101a1f4755383de07cae0c36ea2a07e952c
markdathorne <mark.dathorne@cgi.com>
2022-10-07 18:09:28 +0100

    CCD-3759 Add code coverage

--- src/test/routes/manageWelshDictionary.spec.ts
+++ src/test/routes/manageWelshDictionary.spec.ts
@@ -7,8 +7,20 @@ import * as request from "supertest";
 describe("on POST /manageWelshDictionary", () => {
   const CCD_IMPORT_ROLE = "ccd-import";
 
+  let req;
+  let res;
+  let next;
+
   beforeEach(() => {
     mock.cleanAll();
+    req = {
+      accessToken: "userAuthToken",
+      file: {
+        buffer: new Buffer(8),
+        originalname: "dummy_filename.csv",
+      },
+      serviceAuthToken: "serviceAuthToken",
+    };
   });
 
   it("should respond with Welsh Translation response when authenticated and authorized", () => {
@@ -22,9 +34,7 @@ describe("on POST /manageWelshDictionary", () => {
 
     return request(appTestWithAuthorizedAdminWebRoles)
       .post("/manageWelshDictionary")
-      .send({
-        currentJurisdiction: "TEST", description: "Test draft", version: 1,
-      })
+      .send({ req: req, res: res, next: next})
       .set("Cookie", "accessToken=ey123.ey456")
       .then((res) => {
         expect(res.statusCode).to.equal(200);
