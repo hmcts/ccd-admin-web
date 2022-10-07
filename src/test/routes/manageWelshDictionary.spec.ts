13f2fa11b4f7c8753071cde4398912fcebb4db03
markdathorne <mark.dathorne@cgi.com>
2022-10-07 17:32:49 +0100

    CCD-3759 Add code coverage

--- src/test/routes/manageWelshDictionary.spec.ts
+++ src/test/routes/manageWelshDictionary.spec.ts
@@ -4,6 +4,36 @@ import * as idamServiceMock from "../http-mocks/idam";
 import * as mock from "nock";
 import * as request from "supertest";
 
+describe("on POST /manageWelshDictionary", () => {
+  const CCD_IMPORT_ROLE = "ccd-import";
+
+  beforeEach(() => {
+    mock.cleanAll();
+  });
+
+  it("should respond with Welsh Translation response when authenticated and authorized", () => {
+    it("should return Confirm Delete Definition page when authenticated and authorized", () => {
+    idamServiceMock.resolveRetrieveUserFor("1", CCD_IMPORT_ROLE);
+    idamServiceMock.resolveRetrieveServiceToken();
+
+    mock("http://localhost:4451")
+      .get("/api/idam/adminweb/authorization")
+      .reply(200, {});
+
+    return request(appTestWithAuthorizedAdminWebRoles)
+      .post("/manageWelshDictionary")
+      .send({
+        currentJurisdiction: "TEST", description: "Test draft", version: 1,
+      })
+      .set("Cookie", "accessToken=ey123.ey456")
+      .then((res) => {
+        expect(res.statusCode).to.equal(200);
+      });
+    });
+  });
+
+});
+
 describe("on GET /manageWelshDictionary", () => {
   const CCD_IMPORT_ROLE = "ccd-import";
 
@@ -29,6 +59,6 @@ describe("on GET /manageWelshDictionary", () => {
       .then((res) => {
         expect(res.statusCode).to.equal(200);
       });
+    });
   });
-});
 });
