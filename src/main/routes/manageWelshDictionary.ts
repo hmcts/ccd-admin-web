7ddc9103367b18cac22eaf739f2c0470cccf0e7c
markdathorne <mark.dathorne@cgi.com>
2022-10-12 14:11:45 +0100

    CCD-3759 Refactor manageWelshDictionary script for further code coverage

--- src/main/routes/manageWelshDictionary.ts
+++ src/main/routes/manageWelshDictionary.ts
@@ -3,6 +3,7 @@ import { error_unauthorized_role } from "../util/error_unauthorized_role";
 import { uploadTranslations } from "../service/manage-welsh-dictionary-service";
 import { sanitize } from "../util/sanitize";
 import * as multer from "multer";
+
 const errorPage = "error";
 const welshDictionary = "manageWelshDictionary";
 const router = express.Router();
@@ -18,7 +19,55 @@ const upload = multer({
   storage,
 }).single("file");
 
-router.post(`/${welshDictionary}`, (req, res, next) => {
+function doUploadTranslationsThen(req, res) {
+  const responseContent: { [k: string]: any } = {};
+  responseContent.adminWebAuthorization = req.adminWebAuthorization;
+  responseContent.user = sanitize(JSON.stringify(req.authentication.user));
+  req.session.success = "Successfully uploaded the translations from " + req.file.originalname + ".";
+  responseContent.success = req.session.success;
+  res.render("manageWelshDictionary", responseContent);
+}
+
+function doUploadTranslationsCatch(req, res, error) {
+  req.session.error = {
+    message: error.message ? error.message : "Bad Request",
+    status: error.status ? error.status : 400,
+    text: error.response ? error.response.text : "An error occurred on import",
+  };
+  const responseContent: { [k: string]: any } = {};
+  responseContent.adminWebAuthorization = req.adminWebAuthorization;
+  responseContent.user = sanitize(JSON.stringify(req.authentication.user));
+  responseContent.error = JSON.parse(sanitize(JSON.stringify(req.session.error)));
+  delete req.session.error;
+  res.render("manageWelshDictionary", responseContent);
+}
+
+function doUploadTranslations(req, res) {
+    uploadTranslations(req)
+        .then(() => {
+          doUploadTranslationsThen(req, res);
+        })
+        .catch((error) => {
+          doUploadTranslationsCatch(req, res, error);
+        });
+}
+
+function doGetWelshDictionary(req, res) {
+    res.status(200);
+    const responseContent: { [k: string]: any } = {};
+    responseContent.adminWebAuthorization = req.adminWebAuthorization;
+    responseContent.user = sanitize(JSON.stringify(req.authentication.user));
+    if (req.query.page) {
+        delete req.session.error;
+    }
+    if (req.session.error) {
+        responseContent.error = JSON.parse(sanitize(JSON.stringify(req.session.error)));
+        delete req.session.error;
+    }
+    res.render("manageWelshDictionary", responseContent);
+}
+
+router.post(`/${welshDictionary}`, (req, res) => {
     if (req.adminWebAuthorization &&
     (req.adminWebAuthorization.canLoadWelshTranslation || req.adminWebAuthorization.canManageWelshTranslation)) {
      upload(req, res, (err) => {
@@ -29,50 +78,21 @@ router.post(`/${welshDictionary}`, (req, res, next) => {
         req.session.error = "No file selected! Please select a translations csv file to import";
         res.redirect(302, "/manageWelshDictionary");
       } else {
-        uploadTranslations(req)
-          .then((response) => {
-            const responseContent: { [k: string]: any } = {};
-            responseContent.adminWebAuthorization = req.adminWebAuthorization;
-            responseContent.user = sanitize(JSON.stringify(req.authentication.user));
-            req.session.success = "Successfully uploaded the translations from " + req.file.originalname + ".";
-            responseContent.success = req.session.success;
-            res.render("manageWelshDictionary", responseContent);
-          })
-          .catch((error) => {
-            req.session.error = {
-              message: error.message ? error.message : "Bad Request",
-              status: error.status ? error.status : 400,
-              text: error.response ? error.response.text : "An error occurred on import",
-            };
-            const responseContent: { [k: string]: any } = {};
-            responseContent.adminWebAuthorization = req.adminWebAuthorization;
-            responseContent.user = sanitize(JSON.stringify(req.authentication.user));
-            responseContent.error = JSON.parse(sanitize(JSON.stringify(req.session.error)));
-            delete req.session.error;
-            res.render("manageWelshDictionary", responseContent);
-          });
+          doUploadTranslations(req, res);
       }
     });
-  } else { res.render(errorPage, error_unauthorized_role(req)); }
+  } else {
+        res.render(errorPage, error_unauthorized_role(req));
+  }
 });
 
-router.get(`/${welshDictionary}`, (req, res, next) => {
-if (req.adminWebAuthorization &&
-   (req.adminWebAuthorization.canLoadWelshTranslation || req.adminWebAuthorization.canManageWelshTranslation)) {
-        res.status(200);
-        const responseContent: { [k: string]: any } = {};
-        responseContent.adminWebAuthorization = req.adminWebAuthorization;
-        responseContent.user = sanitize(JSON.stringify(req.authentication.user));
-        if (req.query.page) {
-          delete req.session.error;
-        }
-        if (req.session.error) {
-          responseContent.error = JSON.parse(sanitize(JSON.stringify(req.session.error)));
-          delete req.session.error;
-        }
-        res.render("manageWelshDictionary", responseContent);
-   } else {
-     res.render(errorPage, error_unauthorized_role(req));
-   }
+router.get(`/${welshDictionary}`, (req, res) => {
+    if (req.adminWebAuthorization &&
+    (req.adminWebAuthorization.canLoadWelshTranslation || req.adminWebAuthorization.canManageWelshTranslation)) {
+        doGetWelshDictionary(req, res);
+    } else {
+        res.render(errorPage, error_unauthorized_role(req));
+    }
 });
+
 export default router;
