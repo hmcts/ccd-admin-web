cd542eea95fbf536167da7ae1f73bb735ba9018b
JamesCollettCGI <james.collett@hmcts.net>
2024-03-12 17:33:46 +0000

    Remove stringifyCircularJSON

--- src/main/util/sanitize.ts
+++ src/main/util/sanitize.ts
@@ -3,16 +3,3 @@ const xssFilters = require("xss-filters");
 export function sanitize(data) {
     return xssFilters.inHTMLData(data);
 }
-
-export const stringifyCircularJSON = (obj) => {
-  const seen = new WeakSet();
-  return JSON.stringify(obj, (k, v) => {
-    if (v !== null && typeof v === "object") {
-      if (seen.has(v)) {
-        return;
-      }
-      seen.add(v);
-    }
-    return v;
-  });
-};
