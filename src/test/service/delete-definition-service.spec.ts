39c8c7048be36c3e7ac5b780ab4611baa2afd971
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-02-23 13:58:33 +0000

    Empty commit to retrigger build

--- src/test/service/delete-definition-service.spec.ts
+++ src/test/service/delete-definition-service.spec.ts
@@ -35,6 +35,7 @@ describe("Test Delete Definition service", () => {
       config,
     }).deleteDefinition;
   });
+  
 
   it("should return an HTTP 204 status and success message", (done) => {
     const expectedResult = "Definition deleted successfully";
