ebf8ee158f14ec5b6e1feb08e1a6038e6026a616
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-03-09 12:09:05 +0000

    Revert "Apply changes from CME-736 branch"
    This reverts commit 93ab03100440d1df09fc3f804c36f9adb99bd60c.

--- src/test/service/import-service.spec.ts
+++ src/test/service/import-service.spec.ts
@@ -11,21 +11,24 @@ chai.use(sinonChai);
 describe("importService", () => {
 
   const importUrl = "http://localhost:9999/import";
-  let requestAttachSpy: sinon.SinonSpy;
+  const requestAttachSpy = sinon.spy(request.Request.prototype, "attach");
+  const requestQuerySpy = sinon.spy(request.Request.prototype, "query");
 
   let req;
   let uploadFile;
 
   beforeEach(() => {
-    requestAttachSpy = sinon.spy(request.Request.prototype, "attach");
     req = {
       accessToken: "userAuthToken",
+      body: {},
       file: {
-        buffer: new Buffer(8),
+        buffer: Buffer.from(new Uint8Array(8)),
         originalname: "dummy_filename.abc",
       },
       serviceAuthToken: "serviceAuthToken",
     };
+
+    requestQuerySpy.resetHistory();
 
     const config = {
       get: sinon.stub(),
@@ -35,10 +38,6 @@ describe("importService", () => {
     uploadFile = proxyquire("../../main/service/import-service", {
       config,
     }).uploadFile;
-  });
-
-  afterEach(() => {
-    sinon.restore();
   });
 
   describe("successful file upload", () => {
@@ -54,6 +53,49 @@ describe("importService", () => {
           expect(res.status).to.equal(201);
           expect(res.text).to.equal(expectedResult);
           expect(requestAttachSpy).to.be.calledWith("file", req.file.buffer, { filename: req.file.originalname });
+          expect(requestQuerySpy).to.not.be.called;
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+    });
+
+    it("should set reindex query parameter when reindex is true", (done) => {
+      const expectedResult = "Case Definition data successfully imported";
+      req.body = { reindex: "true" };
+
+      nock("http://localhost:9999")
+        .post("/import")
+        .query({ reindex: true })
+        .reply(201, expectedResult);
+
+      uploadFile(req).then((res) => {
+        try {
+          expect(res.status).to.equal(201);
+          expect(res.text).to.equal(expectedResult);
+          expect(requestQuerySpy).to.be.calledOnce;
+          expect(requestQuerySpy).to.be.calledWith({ reindex: true });
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+    });
+
+    it("should not set query parameters when reindex is false", (done) => {
+      const expectedResult = "Case Definition data successfully imported";
+      req.body = { reindex: "false" };
+
+      nock("http://localhost:9999")
+        .post("/import")
+        .reply(201, expectedResult);
+
+      uploadFile(req).then((res) => {
+        try {
+          expect(res.status).to.equal(201);
+          expect(res.text).to.equal(expectedResult);
+          expect(requestQuerySpy).to.not.be.called;
           done();
         } catch (e) {
           done(e);
