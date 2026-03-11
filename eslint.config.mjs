c7ca85b0c0ca3fdbc0372a136bf94a04b94f51b6
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-03-11 16:09:24 +0000

    Upgrade to node 22 - yarn 4.13 - tsconfig to node-22 - govuk-frontend upgrade from v1 to v5 - swap csurf for @dr.pogodin/csurf - swap tslint for eslint - swap sass-lint for eslint - fixed adminWeb.login_url

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import mocha from "eslint-plugin-mocha";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import tsParser from "@typescript-eslint/parser";
+
+export default defineConfig([
+  {
+    ignores: [
+      "**/config/*",
+      "**/node_modules/*",
+      "yarn.lock",
+      ".yarn/**",
+      "yarn-audit-known-issues",
+      "**/*.properties",
+    ],
+  },
+  { 
+    files: ["**/*.{ts,tsx,js,jsx}"], 
+    plugins: { js, mocha, tseslint }, 
+    extends: [tseslint.configs.recommended, js.configs.recommended], 
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { sourceType: 'module' },
+      ecmaVersion: 2019,
+      sourceType: "module",
+      globals: {
+            ...globals.browser,
+            ...globals.node,
+            ...globals.jquery,
+            ...globals.mocha,
+        },
+    },
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unused-expressions": "off",
+        "no-console": "error",
+        "no-unassigned-vars": "off",
+        "no-unused-vars": "off",
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "comma-dangle": ["error", "always-multiline"],
+        eqeqeq: "error",
+        "require-yield": "off",
+        "mocha/no-exclusive-tests": "error",
+    }
+  },
+  // tseslint.configs.recommended,
+  { 
+    files: ["src/**/*.scss"], 
+    plugins: { css }, 
+    language: "css/css", 
+    extends: ["css/recommended"] 
+  },
+]);
