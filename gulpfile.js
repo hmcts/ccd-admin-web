1deda40f936a12bd1077cf06a7ccc0b70f0206bf
ankita-srivastava009 <125461220+ankita-srivastava009@users.noreply.github.com>
2026-05-11 12:25:57 +0100

    CCD-7101_cve (#783)
    * CCD-7101_cve
    
    * attempt to fix yarn error in pipeline
    
    * updated yarn.lock
    
    ---------
    
    Co-authored-by: Hemanth Potipati <hrpotipati@gmail.com>
    Co-authored-by: dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
    Co-authored-by: Dinesh Patel <dinesh1.patel@btinternet.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 let gulp = require('gulp');
 let nodemon = require('gulp-nodemon');
-let plumber = require('gulp-plumber');
 let livereload = require('gulp-livereload');
 let sass = require('gulp-sass')(require('sass'));
 let path = require('path');
+let { finished } = require('stream/promises');
 
 const repoRoot = path.join(__dirname, '/');
 const govUkFrontendToolkitRoot = path.join(repoRoot, './node_modules/govuk_frontend_toolkit/stylesheets');
@@ -13,38 +13,37 @@ const assetsDirectory = './src/main/public';
 const stylesheetsDirectory = `${assetsDirectory}/stylesheets`;
 
 // compile scss files
-gulp.task('sass', (done) => {
-  gulp.src(stylesheetsDirectory + '/*.scss')
+gulp.task('sass', () => {
+  return gulp.src(stylesheetsDirectory + '/*.scss')
     .pipe(sass({
       includePaths: [
         govUkFrontendToolkitRoot,
         govUkElementRoot
       ]
     }))
-    .pipe(plumber())
-    .pipe(sass())
     .pipe(gulp.dest(stylesheetsDirectory))
     .pipe(livereload());
-  done();
 });
 
 // copy js, stylesheets and images from dependencies to frontend's public directory
-gulp.task('copy-files', (done) => {
-  gulp.src([
-    './node_modules/jquery/dist/jquery.min.js',
-    './node_modules/jquery-validation/dist/jquery.validate.min.js',
-    './node_modules/govuk_frontend_toolkit/javascripts/**/*.js',
-    './node_modules/govuk_template_jinja/assets/javascripts/**/*.js'
-  ])
-    .pipe(gulp.dest(`${assetsDirectory}/js/lib/`));
+gulp.task('copy-files', async () => {
+  await Promise.all([
+    finished(gulp.src([
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/jquery-validation/dist/jquery.validate.min.js',
+      './node_modules/govuk_frontend_toolkit/javascripts/**/*.js',
+      './node_modules/govuk_template_jinja/assets/javascripts/**/*.js'
+    ])
+      .pipe(gulp.dest(`${assetsDirectory}/js/lib/`))),
 
-  gulp.src(['src/main/public/js/lib/**/*.js']).pipe(gulp.dest(`${assetsDirectory}/javascripts`));
+    finished(gulp.src([
+      './node_modules/govuk_template_jinja/assets/stylesheets/**/*'
+    ])
+      .pipe(gulp.dest(`${stylesheetsDirectory}/`)))
+  ]);
 
-  gulp.src([
-    './node_modules/govuk_template_jinja/assets/stylesheets/**/*'
-  ])
-    .pipe(gulp.dest(`${stylesheetsDirectory}/`));
-  done();
+  await finished(gulp.src(['src/main/public/js/lib/**/*.js'])
+    .pipe(gulp.dest(`${assetsDirectory}/javascripts`)));
 });
 
 // compile scss files whenever they're changed
