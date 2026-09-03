0ed7e3910c16b3496a66e180a02d1522f928fd6a
markdathornehmcts <mark.dathorne@hmcts.net>
2026-09-03 10:18:36 +0100

    CCD-7330 Dev review comment - changed

--- src/main/util/fetch.ts
+++ src/main/util/fetch.ts
@@ -1,13 +1,24 @@
-import _fetch from "node-fetch";
+import _fetch, { RequestInfo, RequestInit, Response } from "node-fetch";
 
-export const fetch = (...args) => {
-  return _fetch(...args)
+export class FetchError extends Error {
+  public readonly response: Response;
+  public readonly status: number;
+
+  constructor(response: Response) {
+    super(`HTTP ${response.status} ${response.statusText}`);
+    this.name = "FetchError";
+    this.response = response;
+    this.status = response.status;
+  }
+}
+
+export const fetch = (url: RequestInfo, init?: RequestInit) => {
+  return _fetch(url, init)
     .then((res) => {
-
       if (res.status >= 200 && res.status < 300) {
         return res;
       }
 
-      throw res;
+      throw new FetchError(res);
     });
 };
