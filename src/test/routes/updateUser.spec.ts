526abd2621ea8dcea4c62224b5fff9b113993c8f
markdathornehmcts <mark.dathorne@hmcts.net>
2026-09-03 15:58:47 +0100

    CCD-7330 Fix yarn lint errors

--- src/test/routes/updateUser.spec.ts
+++ src/test/routes/updateUser.spec.ts
@@ -10,35 +10,35 @@ describe("on POST /updateuser", () => {
     mock.cleanAll();
   });
 
-    it("should respond with update user form and populated response when authenticated but not authorized", () => {
-        let backendCalled = false;
-        mock("http://localhost:4451")
-            .get("/api/data/jurisdictions")
-            .optionally()
-            .reply(() => {
-                backendCalled = true;
-                return [200, [{ id: "jd_1", name: "Jurisdiction 1" }, { id: "jd_2", name: "Jurisdiction 2" }]];
-            });
+  it("should respond with update user form and populated response when authenticated but not authorized", () => {
+    let backendCalled = false;
+    mock("http://localhost:4451")
+      .get("/api/data/jurisdictions")
+      .optionally()
+      .reply(() => {
+        backendCalled = true;
+        return [200, [{ id: "jd_1", name: "Jurisdiction 1" }, { id: "jd_2", name: "Jurisdiction 2" }]];
+      });
 
-        return request(appTest)
-            .post("/updateuser")
-            .send({ idamId: "anas@yahoo.com", currentjurisdiction: "test" })
-            .set("Cookie", "accessToken=ey123.ey456")
-            .then((res) => {
-                expect(backendCalled).to.be.false;
-                expect(res.statusCode).to.equal(200);
-                expect(res.text).not.to.contain("Jurisdiction 1");
-                expect(res.text).not.to.contain("Jurisdiction 2");
-                expect(res.text).to.contain("<h2 class=\"heading-large padding\">Unauthorised role</h2>");
-            });
-    });
+    return request(appTest)
+      .post("/updateuser")
+      .send({ idamId: "anas@yahoo.com", currentjurisdiction: "test" })
+      .set("Cookie", "accessToken=ey123.ey456")
+      .then((res) => {
+        expect(backendCalled).to.be.false;
+        expect(res.statusCode).to.equal(200);
+        expect(res.text).not.to.contain("Jurisdiction 1");
+        expect(res.text).not.to.contain("Jurisdiction 2");
+        expect(res.text).to.contain("<h2 class=\"heading-large padding\">Unauthorised role</h2>");
+      });
+  });
 
-    it("should respond with update user form and populated response when authenticated and authorized", () => {
+  it("should respond with update user form and populated response when authenticated and authorized", () => {
       mock("http://localhost:4451")
         .get("/api/data/jurisdictions")
         .reply(200, [{ id: "jd_1", name: "Jurisdiction 1" }, { id: "jd_2", name: "Jurisdiction 2" }]);
 
-    return request(appTestWithAuthorizedAdminWebRoles)
+      return request(appTestWithAuthorizedAdminWebRoles)
       .post("/updateuser")
       .send({idamId: "anas@yahoo.com", currentjurisdiction: "test"})
       .set("Cookie", "accessToken=ey123.ey456")
@@ -49,7 +49,7 @@ describe("on POST /updateuser", () => {
       });
   });
 
-    it("should redirect with error message when invalid email id is passed", () => {
+  it("should redirect with error message when invalid email id is passed", () => {
 
     return request(appTest)
       .post("/updateuser")
@@ -62,7 +62,7 @@ describe("on POST /updateuser", () => {
       });
   });
 
-    it("should redirect with error message when current jurisdiction is empty", () => {
+  it("should redirect with error message when current jurisdiction is empty", () => {
 
     return request(appTest)
       .post("/updateuser")
